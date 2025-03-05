6ee98c1156a2d838ca5b7dbc06277e717c50c751
Jason Suarez <suarezjason@icloud.com>
2025-03-05 18:25:29 +0100

    refact: cleaning up and adding comments

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -7,17 +7,41 @@ import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "@account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) {}
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__CallFailed(bytes returnData);
 
-    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
-        external
-        returns (uint256 validationData)
-    {
-        validationData = _validateSignature(userOp, userOpHash);
-        _payPreFund(missingAccountFunds);
+    IEntryPoint private immutable i_entryPoint;
+
+    /*//////////////////////////////////////////////////////////////
+                                MODIFIER
+    //////////////////////////////////////////////////////////////*/
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
     }
+
+    modifier onlyOwnerOrEntryPoint() {
+        if (msg.sender != owner() && msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    receive() external payable {}
+
+    /*//////////////////////////////////////////////////////////////
+                          INTERNALS-FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
         internal
@@ -35,8 +59,34 @@ contract MinimalAccount is IAccount, Ownable {
 
     function _payPreFund(uint256 missingAccountFunds) internal {
         if (missingAccountFunds > 0) {
-            (bool success,) = payable(msg.sender).call{value: missingAccountFunds}("");
-            require(success, "Failed to pay prefund");
+            (bool success, bytes memory result) = payable(msg.sender).call{value: missingAccountFunds}("");
+            if (!success) {
+                revert MinimalAccount__CallFailed(result);
+            }
         }
+    }
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL-FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        requireFromEntryPoint
+        returns (uint256 validationData)
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        _payPreFund(missingAccountFunds);
+    }
+
+    function execute(address dest, uint256 value, bytes calldata functionData) external {
+        (bool success,) = payable(dest).call{value: value}(functionData);
+        require(success, "Failed to execute");
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                GETTERS
+    //////////////////////////////////////////////////////////////*/
+    function getEntryPoint() public view returns (address) {
+        return address(i_entryPoint);
     }
 }
