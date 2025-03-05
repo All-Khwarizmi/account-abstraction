a93565d530f4bdf7d424084ac278c96b57748449
Jason Suarez <suarezjason@icloud.com>
2025-03-05 15:30:44 +0100

    feat: add internal functions to handle validation of the operation and pay entry point

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,14 +3,40 @@ pragma solidity 0.8.24;
 
 import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "@account-abstraction/contracts/core/Helpers.sol";
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         returns (uint256 validationData)
-    {}
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        _payPreFund(missingAccountFunds);
+    }
 
-    function _validateSignature(bytes32 userOpHash, bytes memory userOpSignature) internal view returns (uint256) {
-        return 1;
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPreFund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds > 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds}("");
+            require(success, "Failed to pay prefund");
+        }
     }
 }
